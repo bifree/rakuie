bdf27ceb23c092e1de7cf747c395d16056089939
bifree <bifree2bfree@gmail.com>
2021-12-04 00:43:57 +0900

    ラクイエ作業開始

---
+++ WordPressTheme/header.php
@@ -0,0 +1,20 @@
+<header>
+  <div class="header">
+    <div class="header-wrapper">
+      <div class="containerTop">
+        <div class="inner">
+          <h1>大阪市で相続不動産売却ならラクイエまで</h1>
+        </div>
+      </div>
+      <div class="containerMiddle">
+        <div class="inner">
+          <div class="box-left">
+            <div class="logo">
+              <img src="" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
