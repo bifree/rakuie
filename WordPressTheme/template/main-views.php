666b4c60e59e003db6c3b722ce52605edd412c3f
Sayaka Miyazaki <>
2021-12-11 02:10:48 +0900

    トップページ

--- WordPressTheme/template/main-views.php
+++ WordPressTheme/template/main-views.php
@@ -3,7 +3,7 @@
         <div class="swiper-wrapper">
             <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/mv1.jpg" alt=""></div>
             <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/mv1.jpg" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/mv1.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/mv3.jpg" alt=""></div>
         </div>
     </div>
 </div>
