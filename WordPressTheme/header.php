30c561a5373201d438a09606d339c1b42becc963
Sayaka Miyazaki <>
2021-12-11 00:17:12 +0900

    no message

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -4,6 +4,7 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/5.4.5/css/swiper.css">
   <?php wp_head(); ?>
 </head>
 
@@ -21,34 +22,23 @@
             <div class="logo">
               <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="">
             </div>
-            <div class="group-button">
-              <div class="yellow-green">
-                <div class="inner">幅広い<br>
-                広告戦略</div>
-              </div>
-              <div class="red">
-                <div class="inner">仲介<br>×<br>買取りMIXプラン</div>
-              </div>
-              <div class="orange">
-                <div class="inner">空き家・空き地<br>無料管理</div>
-              </div>
-            </div>
+            <ul class="group-button">
+              <li><span>幅広い広告戦略</span></li>
+              <li><span>仲介×買取りMIXプラン</span></li>
+              <li><span>空き家・空き地無料管理</span></li>
+            </ul>
           </div>
           <div class="box-right">
-            <dl>
-              <dt>売却専門不動産会社だからできるスキームがあります</dt>
-              <dd>
-                <ul class="contact">
-                  <li class="phone">
-                    <a href="" class="phoneLink">0120-071-778</a>
-                    <span>営業時間 10：00～18：30 | 定休日 水曜日</span>
-                  </li>
-                  <li class="mail">
-                    <a href="">相続不動産売却の<br>無料査定はこちら</a>
-                  </li>
-                </ul>
-              </dd>
-            </dl>
+            <ul class="contact">
+              <li class="phone">
+                <p>無料査定・相続不動産売却に関するご相談</p>
+                <a href="" class="phoneLink">0120-071-778</a>
+                <span>営業時間 10：00～18：30 | 定休日 水曜日</span>
+              </li>
+              <li class="mail">
+                <a href="">相続不動産売却の<br>無料査定はこちら</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
--- WordPressTheme/template/main-views.php
+++ WordPressTheme/template/main-views.php
@@ -1,2 +1,9 @@
 <div class="main-views">
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/mv1.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/mv1.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/mv1.jpg" alt=""></div>
+        </div>
+    </div>
 </div>
