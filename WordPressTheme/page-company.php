6eb8b7374a44191043b6a5d202f9d8cde60accf2
Sayaka Miyazaki <>
2021-12-16 10:16:02 +0900

    会社案内

--- WordPressTheme/page-company.php
+++ WordPressTheme/page-company.php
@@ -15,12 +15,130 @@
         <li>会社案内</li>
       </ol>
     </div>
-
-    <div class="company__content">
-      <h3>ラクイエへのご相談・査定依頼はこちらから</h3>
-    </div>
-
   </div>
+  
+  <div class="container">
+    <div class="company__lead js-inview">
+      <h2>ラクイエのビジョン</h2>
+      <p>日本の相続問題を解決すべく、新たな市場と価値を生み出だし、<br>
+        <strong>「<em>豊かな暮らし</em>」</strong>と<strong>「<em>健康な住環境</em>」</strong>を創造する。</p>
+    </div>
+  </div>
+
+  <div class="company__nav">
+    <div class="container">
+      <ul>
+        <li>
+          <a href="#about">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/sample.jpg" alt="会社概要">
+            <span>会社概要</span>
+          </a>
+        </li>
+        <li>
+          <a href="#message">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/sample.jpg" alt="会社概要">
+            <span>代表挨拶</span>
+          </a>
+        </li>
+        <li>
+          <a href="#access">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/sample.jpg" alt="会社概要">
+            <span>アクセス</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="company__about" id="about">
+    <div class="container">
+      <h2 class="company__title">会社概要</h2>
+      <table>
+        <tr>
+          <th>会社名</th>
+          <td>株式会社ラクイエ - 楽家 -</td>
+        </tr>
+        <tr>
+          <th>所在地</th>
+          <td>〒543-0054 大阪市天王寺区南河堀町1-15</td>
+        </tr>
+        <tr>
+          <th>代表者</th>
+          <td>代表取締役　前田 隆志</td>
+        </tr>
+        <tr>
+          <th>TEL</th>
+          <td>06-6718-5186</td>
+        </tr>
+        <tr>
+          <th>FAX</th>
+          <td>06-6718-5187</td>
+        </tr>
+        <tr>
+          <th>フリーダイヤル</th>
+          <td>0120-071-778</td>
+        </tr>
+        <tr>
+          <th>mail</th>
+          <td>info@rakuie.jp</td>
+        </tr>
+        <tr>
+          <th>資格</th>
+          <td>宅地建物取引士／2級建築施工管理技士／賃貸経営管理士／相続診断士／2級ファイナンシャルプランニング技能士</td>
+        </tr>
+        <tr>
+          <th>事業内容</th>
+          <td>
+            <ul>
+              <li>相続、不動産売買</li>
+              <li>不動産買取</li>
+              <li>建築一式請負、リフォーム業</li>
+              <li>相続不動産コンサルタント業</li>
+            </ul>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+
+  <div class="company__message parallax" id="message">
+    <div class="title">
+      <div class="inner">
+        <h2>代表挨拶</h2>
+      </div>
+    </div>
+    <div class="content">
+      <div class="inner">
+        <div class="company__message--box js-inview">
+          <p>弊社ホームページに起こし頂きありがとうございます。<br>
+            株式会社ラクイエ『不動産相続の窓口』代表の前田隆志と申します。<br>
+            私が相続専門の不動産会社を立ち上げたのは、自身の経験からです。今後、ますます高齢化社会になり人口の減少が続きます。<br>
+            相続問題・空き家問題・地域の過疎化・未登記による所有者不明物件の増加等多くの問題が山積しています。弊社のような相続に特化した不動産会社が、不動産のプロとしてどこよりも正確に情報を発信し、安心、安全に相続をワンストップでサポートさせていただく事で顧客満足、社会貢献、地域活性につながる志しある会社でありたいと考えております。<br>
+            『相続不動産を通して、過疎化や空き家問題を解決し、セーフティネットの底上げを使命と考えます』<br>
+            昨今の相続問題が増加する中で、相続についてのスキーム確率し、士業との連携及び相続不動産ご売却専門会社として、大阪市天王寺区でスタートしました。<br>
+            私自身、建築の学校を卒業し、建築施工管理、設計・リフォーム業・不動産仲介業・新築一戸建販売業・建築請負業・個人不動産家として、20年間『衣・食・住』の住環境に携わって参りましたので、不動産、建築、税金、登記、住宅ローン、リフォームの知識と経験を有しています。どのようなご相談でもその場でご解答させていただきます。<br>
+            弊社では士業ネットワークが確率されたことで、日本の相続問題の解決とセーフティーネットの底上げを使命と考え社会貢献致します。<br>
+            『不動産のプロとして最良のご提案をする事をお約束します。』<br>
+            ※しつこい営業は一切行っておりません。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="company__access" id="access">
+    <div class="container">
+      <h2 class="company__title">アクセス</h2>
+      <div class="company__access--wrap">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d420125.9754026142!2d135.24020243281248!3d34.64878830000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f0.2!3m3!1m2!1s0x6000ddb7145b1b6d%3A0x8d0ec4ee45879a2a!2z5qCq5byP5Lya56S-44Op44Kv44Kk44Ko!5e0!3m2!1sja!2sjp!4v1639232120303!5m2!1sja!2sjp" width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <div>
+          <h3>株式会社ラクイエ</h3>
+          <p>〒543-0054<br>
+					  大阪府大阪市天王寺区南河堀町1-15</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 </main>
 
