e5f5ca9ff14188a19aab06928bcf09d577140011
michi <michiko@michimac.local>
2022-01-08 22:58:13 +0900

    選ばれる理由修正

--- WordPressTheme/page-reason.php
+++ WordPressTheme/page-reason.php
@@ -65,8 +65,38 @@
                             <ul>
                                 <li>ラクイエが窓口となり、司法書士をご紹介。ご不満や問題は、弊社が責任を持ってご対応。</li>
                                 <li>税理士が同席して、相続税などについて無料相談可能。<br>メリット・デメリットを税理士が考慮して、<span class="bold">最良なご提案を一緒に検討。</span></li>
+                                <li>相続税の申告は税理士、不動産の鑑定には不動産鑑定士、相続人同士で揉め事が起きれば弁護士、遺産分割協議書作成は行政書士、不動産の名義変更は司法書士など相続における様々な場面で色々な専門家が必要です。</li>
                             </ul>
                         </div>
+                    </div>
+                    <div class="right js-inview">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/touki.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="reason_5 reason_box">
+                <div class="box_inner">
+                    <div class="left">
+                        <h2 class="title">
+                            <span>スピーディーな売却</span>
+                        </h2>
+                        <p>仲介では物件をお預かりして買主が見つかるまで時間がかかってしまうデメリットがあります。<br>
+                        買主が見つかっても購入希望者は融資を受けて購入することが一般的です。融資審査には1ヶ月以上かかるケースも多く売却決定まで時間がかかります。</br>
+                        <span class="bold">買取では、最短1週間で現金化することもできます。</span></p>
+                    </div>
+                    <div class="right js-inview">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/priority.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="reason_6 reason_box green_bg">
+                <div class="box_inner">
+                    <div class="left">
+                        <h2 class="title">
+                            <span>契約不適合責任なし</span>
+                        </h2>
+                        <p>不動産を仲介で売却する場合、売主様に「契約不適合責任」が発生します。そのため、売買時に外部から発見が困難な建物の傾きや雨漏りなどのについては売却後も売主様が責任を持つ必要があります。<br>
+                        <span class="bold">買取の場合は、「契約不適合責任」は発生しませんので、安心して売却できます。特に築年数が20年以上や相続によりお住まいされていなかった相続人様などは買取方式を選択されると安心です。</span></p>
                     </div>
                     <div class="right js-inview">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/touki.jpg" alt="">
