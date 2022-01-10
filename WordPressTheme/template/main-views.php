ab4e86bc23d80d06dc163ab6f1719ad924428508
bifree <bifree2bfree@gmail.com>
2022-01-10 13:40:14 +0900

    トップ編集

--- WordPressTheme/template/main-views.php
+++ WordPressTheme/template/main-views.php
@@ -2,11 +2,11 @@
     <div class="swiper-container">
         <div class="swiper-wrapper">
             <div class="swiper-slide">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/mv1.png" alt="" class="is_pc">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/mv-pc1.png" alt="" class="is_pc">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/mvsp1.png" alt="" class="is_sp">
             </div>
             <div class="swiper-slide">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/mv2.jpg" alt="" class="is_pc">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/mv2.png" alt="" class="is_pc">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/mvsp2.png" alt="" class="is_sp">
             </div>
             <div class="swiper-slide">
